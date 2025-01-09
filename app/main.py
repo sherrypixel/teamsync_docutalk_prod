1c8bae9064ac1c601f5ddf46adc100abc0f03c2f
ashutosh966 <166472360+ashutosh966@users.noreply.github.com>
2025-01-09 14:19:50 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI,HTTPException, Header, Query
 import uvicorn
 import os
 import doc_process
-import dghi_doc_process
+
 import logging
 
 SERVICE_PORT = int(os.environ["SERVICE_PORT"])
@@ -28,7 +28,6 @@ async def search_documents( text: str = Query(..., description="Search text quer
     path="T"+path.replace("/","_")
     username=username.replace("@","_")
     logging.warning(f"modified path and username is :{path},{username}")
-    
    
     if fileType =='multimedia':
         out_res = doc_process.ES.Audio_Query_search(text,username,path,"audio_text")
@@ -49,7 +48,8 @@ async def search_documents( text: str = Query(..., description="Search text quer
         out_res = doc_process.ES.Image_Query_search(text,username,path)
         search_out = []
         for i in out_res:
-            search_out.append({"fId": i["_source"]["fId"], "score": i["_score"]})
+            if int(i["_score"]) >10:
+                search_out.append({"fId": i["_source"]["fId"], "score": i["_score"]})
         return search_out
         
     result = doc_process.search_documents(text,username,path)
@@ -58,18 +58,15 @@ async def search_documents( text: str = Query(..., description="Search text quer
 # ______________________________________________________________________________________________________
 
 @app.post("/teamsync/nlp/docutalk/search_documents")
-async def search_documents(text,username,modeltype,answerType):
+async def search_documents(text,username,modeltype,answerType,path='T_'):
+    path="T"+path.replace("/","_") if not path.startswith("T_") else path
     username=username.replace("@","_")
     logging.warning(f"{text},{username},{modeltype}")
     if not text:
         raise HTTPException(status_code=400, detail="Query parameter 'text' is required.")
-    if username == "subhash_costacloud.com" : 
-        try:
-            result = dghi_doc_process.dghi_search_documents_gpt(text,username,modeltype)
-        except:
-            result = doc_process.search_documents_gpt(text,username,modeltype,answerType)
+    
     else :
-        result = doc_process.search_documents_gpt(text,username,modeltype,answerType)
+        result = doc_process.search_documents_gpt(text,username,modeltype,answerType,path)
     print("Done...")
     return result
 
@@ -79,12 +76,7 @@ async def search_by_fid(fid,query,modeltype ):
     print(fid)
     if not fid:
         raise HTTPException(status_code=400, detail="File id required")
-    # ------> is logic ke liye
-    if fid in ["675c101a9afdcc779c094a28","675c10329afdcc779c094a32","675c102d9afdcc779c094a2e","675c0fae9afdcc779c094a26","675c10219afdcc779c094a2c","675c0f299afdcc779c094a24","675c10379afdcc779c094a34","675c0f289afdcc779c094a22","675c101f9afdcc779c094a2a"] : 
-        try:
-            result = dghi_doc_process.dghi_Data_By_FID(fid,query,modeltype)
-        except:
-            result = doc_process.Data_By_FID(fid,query,modeltype)
+   
     else :
         result = doc_process.Data_By_FID(fid,query,modeltype)
     print("result")
